9829f1506aca2bab57b2026686447eee0824fd5e
equadras <emanueljose.quadras@gmail.com>
2023-10-18 18:25:15 -0300

    E era uma dp n mt dificil

--- round903/e.cpp
+++ round903/e.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 //#define int long long
+const int inf = 2e5+5;
 
 void solve(){
     int n; cin >> n;
-    vector<int; a(n);
-    for(int i = 0; i < n; i++) cin a[i];
+    vector<int> a(n+1);
+    vector<int> dp(n + 1, inf);
+    dp[0] = 0;
 
-    
+    for(int i = 1; i < n+1; i++) cin >> a[i];
+
+    for(int i = 1; i <= n; i++){
+        if(i + a[i] <= n) dp[i + a[i]] = min(dp[i + a[i]], dp[i - 1]);
+        dp[i] = min(dp[i], dp[i - 1] + 1);
+    }
+    cout << dp[n] << endl;
 }
 
 signed main(){
