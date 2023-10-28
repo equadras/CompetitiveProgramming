5893c2e439b76ffaa73fb3fb1943f20eb8cbfaad
equadras <emanueljose.quadras@gmail.com>
2023-10-28 17:30:49 -0300

    fhsdauids

--- round894/b.cpp
+++ round894/b.cpp
@@ -4,7 +4,20 @@ using namespace std;
 //#define int long long
 
 void solve(){
+    int n; cin >> n;
+    vector<int> a;
+    for(int i = 0; i < n; ++i){
+        int x; cin >> x;
+        if(i && a.back() > x){
+            a.push_back(1);
+        }
+        a.push_back(x);
+    }
+    cout << a.size() << endl;
+    for (int ele : a)
+        cout << ele << " ";
 
+    cout << endl;
 }
 
 signed main(){
