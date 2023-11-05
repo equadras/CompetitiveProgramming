0df58361f6363e6c332683cc8e76d0ee0e11b53b
equadras <emanueljose.quadras@gmail.com>
2023-11-04 23:08:26 -0300

    a facil pra n perder o streak, hj foi puxado

---
+++ round143/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endl "\n"
+//#define int long long
+
+void solve(){
+    int n; cin >> n;
+    int res = 0;
+    for (int i = 0; i < n; i++){
+        int a, b, c; cin >> a >> b >> c;
+        if(a+b+c >= 2) res++;
+    }
+    cout << res << endl; 
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 0;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
