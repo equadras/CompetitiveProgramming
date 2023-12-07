fb654178b18590f2f78a8777dc64c06a756d1b18
equadras <emanueljose.quadras@gmail.com>
2023-12-07 10:49:28 -0300

    tava erado a data

--- round55/a.cpp
+++ round55/a.cpp
@@ -7,20 +7,18 @@ using namespace std;
 void solve(){
     string s; cin >> s;
     string s1, s2;
-    int cnt1 = 0, cnt2 = 0;
+    int x = 0, x1 = 0;
     for (int i = 0; i < size(s); i++){
 		if(islower(s[i]))
-			cnt1++;
+			x++;
 		else
-			cnt2++;
+			x1++;
 		s1 += toupper(s[i]);
 		s2 += tolower(s[i]);
 	}
 
-	if(cnt1 < cnt2)
-		cout << s1;
-	else
-		cout << s2;
+	if(x < x1) cout << s1;
+	else cout << s2;
 }
 
 signed main(){
