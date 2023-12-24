0d083038ee8852e6bf38fc431ac1b8831c9685c5
equadras <emanueljose.quadras@gmail.com>
2023-12-24 11:22:29 -0300

    e1 = e2 mas e diferente ent tai

---
+++ round916/e1.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+typedef long long ll;
+
+//debug
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#ifndef ONLINE_JUDGE
+#define debarr(arr, n) cerr << "arr: ["; forn(i, n) cerr << arr[i] << (i == n-1 ? "" : ", "); cerr << "]" << endl;
+#else
+#define debarr(x...)
+#endif
+
+void solve(){
+    int n; cin >> n;
+    vector<ll> a(n);      
+    vector<ll> b(n);      
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    vector<pair<ll, ll>> mix;
+
+    for (int i = 0; i < n; i++) mix.push_back(make_pair(a[i] + b[i], i));
+
+    sort(mix.rbegin(), mix.rend());
+    ll alice = 0, bob = 0;
+    for (int i = 0; i < n; i++){
+        if(i % 2 == 0) alice += a[mix[i].second] - 1;
+        else bob += b[mix[i].second] - 1;
+        
+    }
+    cout << alice - bob << "\n";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
