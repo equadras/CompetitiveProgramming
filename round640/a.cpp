3d6cef2e6176b650a8fb5dba69fea1c20ad5e434
equadras <emanueljose.quadras@gmail.com>
2023-12-26 14:14:15 -0300

    perdido

--- round640/a.cpp
+++ round640/a.cpp
@@ -1,19 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define endl "\n"
 //#define int long long
 
 void solve(){
-    int n, k; cin >> n >> k;
-    int f = 0;
-    for(int i = 0; i < n; i++){
-        int x; cin >> x;
-        if(x == k){
-            f = true;
-        }
-    }
-    if(f) cout << "YES" << endl;
-    else cout << "NO" << endl;
+
 }
 
 signed main(){
