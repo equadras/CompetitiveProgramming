e1a65da544fad07732f49d353dc198ce89b462ee
equadras <emanueljose.quadras@gmail.com>
2024-01-02 19:48:58 -0300

    puxado

---
+++ educational160/a.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+typedef long long ll;
+
+//debug
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#ifndef ONLINE_JUDGE
+#define debarr(arr, n) cerr << "arr: ["; forn(i, n) cerr << arr[i] << (i == n-1 ? "" : ", "); cerr << "]" << endl;
+#else
+#define debarr(x...)
+#endif
+
+void solve(){
+    string s; cin >> s;
+    int n = s.size();
+
+    int x = 0;
+    int y = 0;
+
+    for (int i = 0; i < n; i++) if (s[x] == '0') x++;
+
+    int num = s[x] - '0';
+
+    int x2 = x + 1;
+    for (int i = 0; i < n; i++){
+        if (s[x2] == '0'){
+            num *= 10;
+            x2++;
+        }
+        if (x2 >= n){
+            cout << -1 << endl;
+            return;
+        }
+    } 
+
+    if (x2 >= n){
+        cout << -1 << endl;
+        return;
+    }
+
+    for (int i = x2; i < n; i++){
+        y = 10 * y + (s[x2] - '0');
+        x2++;
+    } 
+
+    if (y > num){
+        cout << num << ' ' << y << endl;
+    } 
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
