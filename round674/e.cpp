0f5c4f9726ff8d96914ba093512230745a84cd65
equadras <emanueljose.quadras@gmail.com>
2024-02-07 10:59:31 -0300

    questao ruim

--- round674/e.cpp
+++ round674/e.cpp
@@ -8,8 +8,8 @@ typedef long long ll;
 void solve(){
     int n; cin >> n;
     int a1,a2,a3, b1,b2,b3; cin >> a1 >> a2 >> a3 >> b1 >> b2 >> b3;
-    int mi = 0;
-    int ma = min(a1,b2) + min(a2,b3) + min(a3,b1);
+    int mi = 0, ma = 0;
+    ma = min(a1,b2) + min(a2,b3) + min(a3,b1);
     if (a1>b3+b1) mi += (a1-(b1+b3));
     if (a2>b1+b2) mi += (a2-(b1+b2));
     if (a3>b2+b3) mi += (a3-(b2+b3));
