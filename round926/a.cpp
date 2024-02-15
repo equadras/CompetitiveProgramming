537cbb0c34969b4498e9b960fd2a16cb8caef5aa
equadras <emanueljose.quadras@gmail.com>
2024-02-15 19:19:59 -0300

    a do div 2

--- round926/a.cpp
+++ round926/a.cpp
@@ -6,15 +6,24 @@ using namespace std;
 typedef long long ll;
 
 void solve(){
-
+    int n; cin >> n;
+    vector<ll> a(n);
+    ll mx = 0, mn = 1e9+5;
+    for (int i = 0; i < n; i++){
+        int x; cin >> x;
+        a[i] = x;
+        if (x < mn) mn = x; 
+        if (x > mx) mx = x;
+    }
+    cout << mx - mn << endl;
 }
 
 signed main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
-    int TC = 1;
-    if(TC){
-        cin >> TC;
-        while(TC--) solve();
+    int tc = 1;
+    if(tc){
+        cin >> tc;
+        while(tc--) solve();
     } else solve();
     return 0;
 }
