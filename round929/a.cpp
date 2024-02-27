b9356da6a261e5abecce37a83802bc3c303a63a6
equadras <emanueljose.quadras@gmail.com>
2024-02-27 18:28:23 -0300

    a do div3

---
+++ round929/a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    ll n; cin >> n;
+
+    ll res = 0;
+    for (int i = 0; i < n; i++){
+        ll x; cin >> x;
+        res += abs(x);
+    }
+    cout << res << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
+
