3f7cb87ecd2a80280704d3f7bd0d9a52d72e3545
equadras <emanueljose.quadras@gmail.com>
2024-02-27 18:29:13 -0300

    b do div3

---
+++ round929/b.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    vector <int> a(n);
+    int soma = 0;
+
+    for (int i = 0; i < n; i++){
+        int x; cin >> x;
+        a[i] = x;
+        a[i] %= 3;
+        soma += a[i];
+    }
+
+    if (soma % 3 == 0){
+        cout << 0 << endl;
+        return;
+    }
+
+    else if (soma % 3 == 2){
+        cout << 1 << endl;
+        return;
+    } 
+
+    for (int i = 0; i < n; i++){
+        if (a[i] == 1){
+            cout << 1 << endl;
+            return;
+        }
+    }
+
+    cout << 2 << endl;
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
+
