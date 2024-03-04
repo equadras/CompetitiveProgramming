2b56676b49263a7c5c328f0779479f0cf8972438
equadras <emanueljose.quadras@gmail.com>
2024-03-04 09:01:55 -0300

    e do div3 vai saisr ainda

--- round929/e.cpp
+++ round929/e.cpp
@@ -12,7 +12,35 @@ using namespace std;
 typedef long long ll;
 
 void solve(){
+    int n; cin >> n;
+    vector<int> a(n);
+    vector<int> pref(n+1);
+    
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        pref[i+1] = pref[i] + a[i];
+    }
 
+    int q; cin >> q;
+    int l, u;
+    int idx = -1;
+    while(q--){
+        cin >> l >> u;
+        int ll = l;
+        int rr = n; 
+
+        while (ll <= rr){
+            int mid = (ll + rr) / 2;
+            if (pref[mid] - pref[l-1] > (u+1)){
+                rr = mid - 1;
+            }
+            else {
+                idx = mid;
+                l = mid+1;
+            }
+        }
+    }
+    cout << res << " " << pref[res] << endl;
 }
 
 signed main(){
