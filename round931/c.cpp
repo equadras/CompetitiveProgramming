90b7d8178489bec5f83642a10b22c826beed7fbe
equadras <emanueljose.quadras@gmail.com>
2024-03-05 18:48:42 -0300

    nossa que C legal e impossivel ao mesmo tempo

--- round931/c.cpp
+++ round931/c.cpp
@@ -11,67 +11,48 @@ using namespace std;
 #define endl "\n"
 typedef long long ll;
 
-pair<int,int> calc(int n, int m, int x, int y){
-    pair<int,int> b = {-1,-1};
-    int yy = x-y+m-1;
-    if (yy &1) return b;
-    else {
-        yy /= 2;
-        int xx = x-yy;
-        if (xx < 0 || yy < 0 || xx >= n || yy >= m) return b;
-        else {
-            pair<int,int> cord = {xx+1,yy+1};
-            return cord;
-        }
-    }
+
+pair<ll,ll> calc(ll n, ll m, ll x, ll y){
+    ll yy = (x-y+m-1);
+    if (yy&1) return {-1,-1};
+    yy/=2;
+    ll xx = x-yy;
+    if (xx < 0 || yy < 0 || xx >= n || yy >= m) return {-1,-1};
+    return {xx+1,yy+1};
 }
 
 void solve(){
-    int n,m;
-    cin >> n >> m;
+    ll n,m; cin >> n >> m;
 
     cout << "? " << 1 << " " << 1 << endl;
 
-    int x;
-    cin >> x;
+    ll x; cin >> x;
 
     cout << "? " << n << " " << m << endl;
-    int y;
-    cin >> y;
+    ll y; cin >> y;
     y = n+m-2-y;
 
     cout << "? " << 1 << " " << m << endl;
-    int z;
-    cin >> z;
+    ll z; cin >> z;
 
-    pair<int,int> m1 = calc(n,m,x,z);
-    pair<int,int> m2 = calc(n,m,y,z);
+    pair<ll,ll> m1 = calc(n,m,x,z);
+    pair<ll,ll> m2 = calc(n,m,y,z);
 
-    if (m2.first == 01){
-        cout << "! " << m1.first << " " << m1.second << endl;
-        return;
-    }
-    cout << "? " <<m2.first << " " << m2.second << endl;
-    cin >> x;
-
-    if (!x) cout << "! " << m2.first << " " << m2.second << endl;
-    else cout << "! " << m1.first << " " << m1.second << endl;
-
-    if (m1.first == -1){
+    if(m1.first == -1){
         cout << "! " << m2.first << " " << m2.second << endl;
         return;
     }
 
     cout << "? " <<m1.first << " " << m1.second << endl;
     cin >> x;
-
-    if (x == 0) cout << "! " << m1.first << " " << m1.second << endl;
+    if(x == 0) cout << "! " << m1.first << " " << m1.second << endl;
     else cout << "! " << m2.first << " " << m2.second << endl;
 }
 
+
 signed main(){
-    /* ios_base::sync_with_stdio(0);cin.tie(0); */
-    int TC = 0;
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
     if(TC){
         cin >> TC;
         while(TC--) solve();
