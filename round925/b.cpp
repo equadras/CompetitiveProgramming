69dfb1c4963e2a5641493aca20d82e653a33364b
equadras <emanueljose.quadras@gmail.com>
2024-03-18 15:21:54 -0300

    b que deu mais trabalho doq devia

--- round925/b.cpp
+++ round925/b.cpp
@@ -12,7 +12,29 @@ using namespace std;
 typedef long long ll;
 
 void solve(){
+    int n; cin >> n;
+    vector<int> a(n);
+    int sm = 0;
+    for (int i = 0; i < n; i++){ 
+        cin >> a[i];
+        sm += a[i];
+    }
 
+    int sobra = 0;
+    for (int i = 0; i < n; i++){ 
+
+        if (a[i] > sm/n) sobra += a[i] - (sm/n);
+        else sobra -= (sm/n) - a[i];
+
+        if (sobra < 0){
+            cout << "NO" << endl;
+            return;
+        }
+
+    }
+
+    cout << "YES" << endl;
+        
 }
 
 signed main(){
