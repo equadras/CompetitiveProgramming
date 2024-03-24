ad9057dbd5646745823c41acd0b55174cfd489ce
equadras <emanueljose.quadras@gmail.com>
2024-03-24 14:54:27 -0300

    b

---
+++ round935/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    ll a,b,m;
+    cin >> a >> b >> m;
+    ll res = 2+m/a+m/b;
+    cout << res << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
