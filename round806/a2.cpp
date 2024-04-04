3393733ae8827776930686426261e4cb6c62e559
equadras <emanueljose.quadras@gmail.com>
2024-04-04 19:28:25 -0300

    refazendo um div4 antigo

---
+++ round806/a2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    string s; cin >> s;
+
+    transform(s.begin(), s.end(), s.begin(),::toupper);
+    cout << (s == "YES" ? "YES" : "NO") << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
+
