a79e768b775b4a3e106a43f0f6870c6d76da5694
equadras <emanueljose.quadras@gmail.com>
2024-04-04 16:21:54 -0300

    f

--- round937/f.cpp
+++ round937/f.cpp
@@ -11,36 +11,35 @@ using namespace std;
 #define endl "\n"
 typedef long long ll;
 
+
 void solve(){
     int a, b, c; cin >> a >> b >> c;
     if (a+1 != c){
         cout << -1 << endl;
         return;
     }
+    int res = -1, cnt = 1;
+
+    int x,k,l;
+    while (a + b + c > 0){
+        if (a < cnt) x = a;
+        if (cnt <= a) x = cnt;
 
 
-    int res = 0;
-    int nodo_agr = 1;
-    int nodo = 1;
+        if (b < cnt-x) k = b;
+        if (cnt-x <= b) k = cnt-x;
 
-    int cnt = 0;
-    while (true){
-		if (!nodo_agr){
-	        res++;
-            /* debug(nodo_agr,nodo); */
-            swap(nodo_agr,nodo);
-	    }
-	    nodo_agr--;
-	    nodo++;
-        if (cnt < a) nodo++;
 
-        debug(nodo_agr,nodo);
+        if (c < cnt-x-k) l = c;
+        if (cnt-x-k <= c) l = cnt-x-k;
 
-        cnt++;
-        if (cnt >= a+b) break;
-	}
+        a -= x; b -= k; c -= l;
+        cnt = cnt + x - l;
 
-	cout << res << endl;
+        res++;
+    }
+
+    cout << res << endl;
 }
 
 signed main(){
