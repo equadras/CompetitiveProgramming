c8c0c1fdc55a1027976acc8cbf874b05c0fba35b
equadras <emanueljose.quadras@gmail.com>
2024-04-13 23:26:17 -0300

    fechei

---
+++ Contests udesc/bruteclass24.1-2/g.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+
+    map<char,int> mp;
+
+    int res = 0;
+
+    for (int i = 0 ;i < n; i ++){
+        char c = s[i];
+        mp[c]++;
+
+        (mp[c] != 1 ? res ++ : res += 2);
+
+    }
+    cout << res << endl;
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
+
+
