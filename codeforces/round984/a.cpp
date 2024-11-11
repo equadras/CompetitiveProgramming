539b78e5fe6381e285bb1f9e1fb4d49aab016e0e
equadras <emanueljose.quadras@gmail.com>
2024-11-11 18:19:15 -0300

    a

---
+++ codeforces/round984/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for (int i = 1; i < n; i++){
+        int d = abs(a[i] - a[i-1]);
+        if (d != 5 && d != 7){
+            cout << "NO" << endl;
+            return;
+        } 
+    }
+    cout << "YES" << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
+
+
