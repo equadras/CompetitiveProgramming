ae135ef2d46bdf0a8858b9234c8b3f60cb29ad4b
equadras <emanueljose.quadras@gmail.com>
2025-04-24 20:09:43 -0300

    commit mid contest pq n sei mais

---
+++ codeforces/round1020/a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL_DEBUG
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define endl "\n"
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+    int cnt = 0; 
+
+    for (int i = 0; i < n; i++) if (s[i] == '0') cnt++;
+    cout << (n-cnt) * n + cnt - (n-cnt) << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if(TC){
+        cin >> TC;
+        while(TC--) solve();
+    } else solve();
+    return 0;
+}
+
+
+
+
